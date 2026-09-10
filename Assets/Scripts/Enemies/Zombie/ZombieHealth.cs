2a2f9d48f8d027662914c91bf2f079d0e8ece3b4
BUNNYOI <ndnhana24103@cusc.ctu.edu.vn>
2026-09-10 23:53:20 +0700

    feat(enimie/zombie): implement zombie movement

---
+++ Assets/Scripts/Enemies/Zombie/ZombieHealth.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+//quản lý máu và xử lý sát thương của zombie
+public class ZombieHealth : MonoBehaviour, IDamageable
+{
+    //lượng máu tối đa của zombie
+    [SerializeField] private float maxHealth = 100f;
+
+    //lượng máu hiện tại của zombie
+    private float currentHealth;
+
+    private ZombieDeath zombieDeath;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+        zombieDeath = GetComponent<ZombieDeath>();
+    }
+
+    //xử lý khi zombie nhận sát thương
+    public void TakeDamage(float damage)
+    {
+        if (damage <= 0f)
+        {
+            return;
+        }
+
+        currentHealth -= damage;
+
+        currentHealth = Mathf.Max(currentHealth, 0f);
+
+        if (currentHealth <= 0)
+        {
+            zombieDeath.Die();
+        }
+        Debug.Log(
+            $"Zombie took {damage} damage. " +
+            $"HP: {currentHealth}/{maxHealth}"
+        );
+    }
+
+    //test nhận sát thương
+    private void Update()
+    {
+        //nhấn space để test nhận sát thương
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            TakeDamage(10f);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/Zombie/ZombieMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    //tốc độ di chuyển
+    [SerializeField] private float movementSpeed = 1f;
+
+    //điểm cần di chuyển tới
+    [SerializeField] private Transform targetTransform;
+
+    //vị trí cần di chuyển tới
+    private Vector3 targetPosition;
+
+    //đặt vị trí cần di chuyển tới
+    public void MoveTo(Vector3 newTargetPosition)
+    {
+        targetPosition = newTargetPosition;
+    }
+
+    private void Start()
+    {
+        MoveTo(targetTransform.position);
+    }
+
+    private void Update()
+    {
+        //di chuyển zombie về phía vị trí mục tiêu
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            targetPosition,
+            movementSpeed * Time.deltaTime
+        );
+    }
+
+    
+}
